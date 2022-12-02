f05fa044a04b95abb9832edb9fdc495d191977f5
Renato Diniz <dinizre@hotmail.com>
2022-12-02 11:03:59 -0300

    Desafio 01 - Introdução ao SOLID

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,21 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+    if (!user.admin) {
+      throw new Error('User isn\'t an admin');
+    }
+
+    const list = this.usersRepository.list();
+    return list;
   }
 }
 
